49ba64712c3e2494f2861360b985b9bea10a749c
stassa <ep50@uni.brighton.ac.uk>
2015-07-19 18:22:22 +0100

    Cleaned language & examples modules for hand-simulation mtg example.

--- supergrammar/examples_mtg_hand_simulation.pl
+++ supergrammar/examples_mtg_hand_simulation.pl
@@ -1,61 +1,5 @@
-:-module(examples_mtg_hand_simulation, [language//0
-				       ,start//0
-				       ,terminal//0
-				       ,terminals//0
-				       ,nonterminal//0
-				       ,nonterminals//0
-				       ,example//0
-				       ,example_string/1]).
+:-module(examples_mtg_hand_simulation, [example_string/1]).
 
-:-dynamic nonterminal//0.
-
-%!	language// is nondet.
-%
-%	A high-level specification of a language type.
-language --> nonterminal.
-
-
-
-start --> [ability].
-
-
-
-%!	terminals// is nondet.
-%
-%	Zero or more terminals; part of background knowledge of the
-%	target language.
-terminals --> [].
-terminals --> terminal, terminals.
-
-nonterminals --> [].
-nonterminals --> nonterminal, nonterminals.
-
-%!	terminal// is nondet.
-%
-%	A list of terminals; part of bakground knowledge of the target
-%	langauge.
-%
-%	Note how tokens are wrapped in double square brackets: [[]].
-%	This is so bounded_derivation/4 adds them as terminals in the
-%	body of a new rule.
-%
-terminal --> [[]].
-
-
-%!	nonterminal// is nondet.
-%
-%	Exactly one nonterminal symbol; part of bakground knowledge of the target
-%	langauge.
-%
-%	These are only the names of nonterminals in our partial grammar.
-%	They are wrapped in single square brackets, [], so they will be
-%	added as nonterminals (without brackets) in the body of a new
-%	rule by bounded_derivation/4.
-%
-nonterminal --> [].
-
-
-example --> {example_string(S)}, S.
 
 %!	examples_string(?Example) is nondet.
 %
@@ -63,3 +7,12 @@ example --> {example_string(S)}, S.
 example_string([destroy,target,creature]).
 example_string([destroy,target,artifact]).
 example_string([exile,target,creature]).
+
+
+
+
+
+
+
+
+
