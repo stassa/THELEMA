f00fa9a7331850adfdc0b0763b3752ebc5487013
stassa <ep50@uni.brighton.ac.uk>
2015-09-20 23:09:06 +0100

    Grammar evaluation module (empty).

--- tree_learning/load_tree_learning.pl
+++ tree_learning/load_tree_learning.pl
@@ -12,6 +12,7 @@ user:file_search_path(output, tree_learning(output)).
 :-use_module(language).
 :-use_module(production_induction).
 :-use_module(grammar_printing).
+:-use_module(grammar_evaluation).
 
 %:-use_module(production_induction_v1).
 %:-use_module(grammar_printing_v1).
@@ -24,6 +25,7 @@ edit_source:-
 	edit(tree_learning(load_tree_learning))
 	,edit(tree_learning(production_induction))
 	%,edit(tree_learning(production_induction_v1))
+	,edit(tree_learning(grammar_evaluation))
 	,edit(tree_learning(grammar_printing))
 	%,edit(tree_learning(grammar_printing_v1))
 	,edit(tree_learning(load_configuration))
