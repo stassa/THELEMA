d09075868d8346287fa366bc9c2481514159f20b
spatsant <stassa.patsantzis@aexp.com>
2015-09-28 16:59:50 +0100

    Still working on printing a compression grammar. * Added some parameters to printing predicates; also some comments etc.

--- tree_learning/grammar_printing.pl
+++ tree_learning/grammar_printing.pl
@@ -46,12 +46,20 @@ print_grammar(tree):-
 	,configuration:output_stream(O)
 	,expand_file_search_path(O, P)
 	,open(P,write,Stream,[])
-	,once(print_grammar_file(Stream, S, Ps))
+	,once(print_grammar_file(tree, Stream, S, Ps))
 	,close(Stream)
 	,edit(P).
 
 
-print_grammar_file(Stream, S, Ps):-
+%!	print_grammar_file(+Type,+Stream,+Start,-Productions) is det.
+%
+%	Print a Prolog file with derived productions in DCG notation.
+%	Start is the start symbol in the derived grammar; Type is the
+%	value of configuration option grammar_printing/1 in herited (!)
+%	from print_grammar/1. Something something closure Type
+%	something.
+%
+print_grammar_file(tree, Stream, S, Ps):-
 	stream_property(Stream, file_name(Path))
 	,file_base_name(Path, Filename)
 	,file_name_extension(Grammar_module_name, _Ext, Filename)
@@ -60,6 +68,16 @@ print_grammar_file(Stream, S, Ps):-
 	,write(Stream, '\n')
 	,forall(member(P, Ps), print_term(Stream, p, P)).
 
+
+%!	print_term(+Start,+Type,+Term) is det.
+%
+%	convenience wrapper around write_term/3 to reduce boilerplate
+%	when printing productions.
+%
+%	Start is the start symbol in the derived grammar; Type is one
+%	of: [s,p,t,n] (start, production, terminal, nonterminal) and
+%	determines the type of term to print; Term is the term to print.
+%
 print_term(S, p, T):-
 	write_term(S, T,[fullstop(true),nl(true),spacing(next_argument),quoted(true)]).
 
