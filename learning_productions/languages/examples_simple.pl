358096320bea61dbe6c523eb03ee7a50880fcc79
stassa <ep50@uni.brighton.ac.uk>
2015-10-10 23:54:05 +0100

    Cleaned up dev stuff; previous versions and so on.

--- learning_productions/languages/examples_simple.pl
+++
@@ -1,8 +0,0 @@
-:-module(examples_simple, [example_string/1
-			  ]).
-
-% A set of example strings that we know belong to the language
-example_string([a]).
-example_string([a,b]).
-example_string([a,b,c]).
-
--- rule_machine/main.pl
+++
@@ -1,7 +0,0 @@
-
-:-use_module(rule_machine). % Load first to stick multifile state/3.
-:-use_module(mtg_rule_engine).
-
-:-edit('main.pl').
-:-edit('rule_machine.pl').
-:-edit('mtg_rule_engine.pl').
